c744fcd4e688ac74bbb276bea9389c89acb1c49d
JayuSC2 <134278095+JayuSC2@users.noreply.github.com>
2023-11-13 19:54:46 +0100

    Add files via upload

--- Level 03/tab_mult.c
+++ Level 03/tab_mult.c
@@ -12,21 +12,50 @@
 
 #include <unistd.h>
 
-int	tab_mult(int num)
+int ft_atoi(char *str)
 {
-	int *tab;
-	int	i = 1;
-	
-	while (i < 10)
+	int num = 0;
+
+	while (*str)
+		num = num * 10 + *str++ - '0';
+	return (num);
+}
+
+void	ft_putnbr(int num)
+{
+	char	digit;
+
+	while (num > 9)
 	{
-		tab[i] = num * i;
-		i++;
+		ft_putnbr(num / 10);
+		num = num % 10;
 	}
+	digit = num + '0';
+	write (1, &digit, 1);
+}
+
 int main(int argc, char **argv)
 {
 	int i = 1;
+	int	num = 0;
 
-	if (argc == 1)
+	if (argc != 2)
 		write (1, "\n", 1);
+	else
+	{
+		num = ft_atoi(argv[1]);
+		while (i < 10)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(num);
+			write(1, " = ", 3);
+			ft_putnbr(i * num);
+			write (1, "\n", 1);
+			i++;
+		}
+		return (0);
+	}
+}
 	
 	
