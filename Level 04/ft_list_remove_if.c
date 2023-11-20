7f8d6bd6a8227700c52a2373a3268c4ced9351aa
JayuSC2 <134278095+JayuSC2@users.noreply.github.com>
2023-11-20 15:52:00 +0100

    Add files via upload

---
+++ Level 04/ft_list_remove_if.c
@@ -0,0 +1,17 @@
+#include "ft_list.h"
+#include <stdlib.h>
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	if (begin_list == NULL || *begin_list == NULL)
+		return (NULL);
+	t_list *current = begin_list;
+	if ((*cmp)(data_ref, current->data == 0))
+	{
+		*begin_list = current->next;
+		free(current);
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+	current = *begin_list;
+	ft_list_remove_if(&current->next, data_ref, cmp);
+}
